7b9f8cbe738276ec3ded925e5ed483f14c2cbfac
vu.huy.tuan <SUN-ASTERISK\vu.huy.tuan@b120174-pc.sun-asterisk.com>
2020-07-29 19:19:37 +0700

    Lesson3: List Building, with style

--- rn-starter/App.js
+++ rn-starter/App.js
@@ -2,15 +2,17 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from "./src/screens/HomeScreen";
 import ComponentsScreen from "./src/screens/ComponentsScreen";
+import ListScreen from "./src/screens/ListScreen";
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
     ComponentsScreen: ComponentsScreen,
+    ListScreen: ListScreen,
 
   },
   {
-    initialRouteName: "ComponentsScreen",
+    initialRouteName: "ListScreen",
     defaultNavigationOptions: {
       title: "App"
     }
