ca41c298d3637840f6067e8bece5636b5a4799c7
vu.huy.tuan <vu.huy.tuan@framgia.framgia.com>
2020-08-02 23:12:07 +0700

    COLOR INCREMENT

--- rn-starter/src/screens/SquareScreen.js
+++ rn-starter/src/screens/SquareScreen.js
@@ -8,25 +8,51 @@ const SquareScreen = (props) => {
     const [red, setRed] = useState(0);
     const [blue, setBlue] = useState(0);
     const [green, setGreen] = useState(0);
+
+    const setColor = (color, change) => {
+
+        switch (color) {
+            case 'red':
+//                if (red + change > 255 || red + change < 0) {
+//                    return;
+//                } else {
+//                    setRed(red + change);
+//                }
+                red + change > 255 || red + change < 0 ? null : setRed(red + change);
+                return;
+
+            case 'green':
+                green + change > 255 || green + change < 0 ? null : setGreen(green + change);
+                return;
+
+            case 'blue':
+                blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change);
+                return;
+
+            default:
+                return;
+        }
+    };
 console.log(red);
+
     return (
         <View>
             <ColorCounter
                 color='Red'
-                onIncrease={ () => setRed(red + COLOR_INCREMENT)}
-                onDecrease={ () => setRed(red - COLOR_INCREMENT)}
+                onIncrease={ () => setColor('red', COLOR_INCREMENT)}
+                onDecrease={ () => setColor('red', -1 * COLOR_INCREMENT)}
                 />
 
             <ColorCounter
                 color='Blue'
-                onIncrease={ () => setBlue(blue + COLOR_INCREMENT)}
-                onDecrease={ () => setBlue(blue - COLOR_INCREMENT)}
+                onIncrease={ () => setColor('blue', COLOR_INCREMENT)}
+                onDecrease={ () => setColor('blue', -1 * COLOR_INCREMENT)}
                 />
 
             <ColorCounter
                 color='Green'
-                onIncrease={ () => setGreen(green + COLOR_INCREMENT)}
-                onDecrease={ () => setGreen(green - COLOR_INCREMENT)}
+                onIncrease={ () => setColor('green', COLOR_INCREMENT)}
+                onDecrease={ () => setColor('green', -1 * COLOR_INCREMENT)}
                 />
 
             <View style={{
