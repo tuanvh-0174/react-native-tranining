de701f595019ce4d4c9661150db44a3492797ffe
vu.huy.tuan <vu.huy.tuan@framgia.framgia.com>
2020-08-08 16:27:31 +0700

    Install react-navigator

--- food/App.js
+++ food/App.js
@@ -1,8 +1,141 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import * as React from 'react';
+import { View, Button, Text, Animated } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import SearchScreen from './src/screens/SearchScreen';
 
-const navigator = createStackNavigator(
-{
+
+// In App.js in a new project
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={SearchScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+/*
+
+function Home({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home screen</Text>
+
+      <Button
+        title="Go to Profile"
+        onPress={() => navigation.navigate('SearchScreen')}
+      />
+    </View>
+  );
+}
+
+function Profile({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Profile screen</Text>
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+const forFade = ({ current, next }) => {
+  const opacity = Animated.add(
+    current.progress,
+    next ? next.progress : 0
+  ).interpolate({
+    inputRange: [0, 1, 2],
+    outputRange: [0, 1, 0],
+  });
+
+  return {
+    leftButtonStyle: { opacity },
+    rightButtonStyle: { opacity },
+    titleStyle: { opacity },
+    backgroundStyle: { opacity },
+  };
+};
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerTintColor: 'white',
+          headerStyle: { backgroundColor: 'tomato' },
+        }}
+      />
+      <Stack.Screen
+        name="Profile"
+        component={Profile}
+        options={{ headerStyleInterpolator: forFade }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
+*/
+
+
+
+// In App.js in a new project
+/*
+
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+*/
+
+/*import { createStackNavigator, createAppContainer } from 'react-navigation';
+import SearchScreen from './src/screens/SearchScreen';
+
+const navigator = createStackNavigator({
     Search: SearchScreen,
 },
 {
@@ -13,7 +146,7 @@ const navigator = createStackNavigator(
 }
 );
 
-export default createAppContainer(navigator);
+export default createAppContainer(navigator);*/
 
 /*
 import { StatusBar } from 'expo-status-bar';
