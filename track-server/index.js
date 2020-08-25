fbaa74a920eb7cdcffc0786910e637498cbfc722
vu.huy.tuan <SUN-ASTERISK\vu.huy.tuan@b120174-pc.sun-asterisk.com>
2020-08-25 18:42:39 +0700

    Init server, mongoosedb

---
+++ track-server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+const mongoUri = 'mongodb+srv://mogoadmin:mYIpUnlw6tlVrhtt@cluster0.q80w7.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology:true,
+});
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance');
+});
+mongoose.connection.error('error', (err) => {
+    console.log('Error connecting to mongo', err);
+});
+
+app.get('/', (req, res) => {
+     res.send('Hi there !');
+});
+
+app.listen(3000, () => {
+   console.log('Listening on port 3000');
+});
